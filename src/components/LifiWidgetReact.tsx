f3fee5f986049d08076bb2f309c8926b9b66e9d9
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-03-26 13:05:16 +0700

    update change rpc

--- src/components/LifiWidgetReact.tsx
+++ src/components/LifiWidgetReact.tsx
@@ -14,8 +14,11 @@ function LifiWidgetReact({ config, integrator }: any) {
     document.dispatchEvent(event);
   }
   const sdkConfig = {
-    apiUrl:'https://staging.li.quest/v1'
+    apiUrl:'https://staging.li.quest/v1',
+    rpcs: 
+      { 5: ['https://goerli.blockpi.network/v1/rpc/public'],  80001 : ['https://rpc.ankr.com/polygon_mumbai'] }
   }
+  
   const widgetConfig = {
      featured: [
        {
@@ -27,24 +30,40 @@ function LifiWidgetReact({ config, integrator }: any) {
         logoURI: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21334.png',
       },
       {
-        address: '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c',
-        symbol: 'USDC',
+        address: '0x64ef393b6846114bad71e2cb2ccc3e10736b5716',
+        symbol: 'USDT',
         decimals: 18,
         chainId: 5,
+        name: 'USDT',
+        logoURI: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21334.png',
+      },
+       {
+        address: '0x0000000000000000000000000000000000000000',
+        symbol: 'MATIC',
+        decimals: 18,
+        chainId: 80001,
+        name: 'MATIC',
+        logoURI: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21334.png',
+      },
+      {
+        address: '0x0FA8781a83E46826621b3BC094Ea2A0212e71B23',
+        symbol: 'USDC',
+        decimals: 18,
+        chainId: 80001,
         name: 'USDC',
         logoURI: 'https://s2.coinmarketcap.com/static/img/coins/64x64/21334.png',
       },
     ],
     // set source chain to Polygon
-    fromChain: 5,
+    fromChain: 80001,
     // set destination chain to Optimism
-    toChain: 5,
+    toChain: 80001,
     //  // ETH
     fromToken: '0x0000000000000000000000000000000000000000',
     // set source token to USDC (Optimism)
-    toToken: '0xd87ba7a50b2e7e660f678a895e4b72e7cb4ccd9c',
+    toToken: '0x0FA8781a83E46826621b3BC094Ea2A0212e71B23',
     // set source token amount to 10 USDC (Polygon)
-    fromAmount: 1,
+    fromAmount: 0.0001,
   };
    useEffect(() => {
     const onRouteExecutionStarted = (route: Route) => {
